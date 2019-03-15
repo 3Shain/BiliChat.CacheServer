4f74bcad789ddc72b2b17b1571a101b12e7c3c34
3Shain <san3shain@outlook.com>
2019-03-15 10:11:12 +0800

    统计去重

--- src/SanShain.Bilichat/Controllers/HistoryController.cs
+++ src/SanShain.Bilichat/Controllers/HistoryController.cs
@@ -20,8 +20,8 @@ namespace SanShain.Bilichat.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
-            return Json(await Db.Entrys.OrderByDescending(x => x.id).Take(30).Select(u =>
-              new { u.user_id, u.user_intro, u.user_name, u.room_id, u.id, time = (u.entry_time.Ticks - (new DateTime(1970, 1, 1, 0, 0, 0)).Ticks) / 10000 }).ToArrayAsync());//时区代码很有问题,以后改
+            return Json(await Db.Entrys.OrderByDescending(x => x.entry_time).GroupBy(x => x.room_id).Take(20).Select(key => key
+              .Select(u => new { u.user_id, u.user_intro, u.user_name, u.room_id, u.id, time = (u.entry_time.Ticks - (new DateTime(1970, 1, 1, 0, 0, 0)).Ticks) / 10000 }).FirstOrDefault()).ToArrayAsync());
         }
     }
 }
