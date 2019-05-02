fa95523665b55f590145b258bd69305315284c53
3Shain <san3shain@outlook.com>
2019-05-02 19:55:41 +0800

    /api/avturl

---
+++ src/SanShain.Bilichat/Controllers/AvatarUrlController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SanShain.Bilichat.Services;
+
+namespace SanShain.Bilichat.Controllers
+{
+    [Route("api/avturl")]
+    [ApiController]
+    public class AvatarUrlController : Controller
+    {
+        public CacheService Cache { get; set; }
+
+        [HttpGet("{mid}")]
+        public async Task<IActionResult> Get(int mid)
+        {
+            if (mid == 0)
+            {
+                return BadRequest();
+            }
+            var ret = await Cache.GetUserInfo(mid);
+            if (ret != null)
+            {
+                Response.Headers["Cache-Control"] = "public,max-age=" + 60 * 60 * 24 * 3;
+                return Json(new
+                {
+                    ret.face
+                });
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        public AvatarUrlController(CacheService cache)
+        {
+            Cache = cache;
+        }
+    }
+}
